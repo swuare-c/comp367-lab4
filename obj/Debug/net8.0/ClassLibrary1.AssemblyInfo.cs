c45cc795567131265e439284d89958f66082dd74
Christian Khan <christiankhan100@gmail.com>
2026-04-18 16:56:14 -0400

    Update ClassLibrary1.csproj

--- obj/Debug/net8.0/ClassLibrary1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClassLibrary1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e7cc6e0df72e19dabea88e7485c246cbaf16a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f25f0a3d835a309582a604bcd91d7171f7d57af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
