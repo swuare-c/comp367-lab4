85e7cc6e0df72e19dabea88e7485c246cbaf16a4
Christian Khan <christiankhan100@gmail.com>
2026-04-18 15:30:04 -0400

    first commit

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary1
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) return false;
+            return char.IsUpper(str[0]);
+        }
+    }
+}
